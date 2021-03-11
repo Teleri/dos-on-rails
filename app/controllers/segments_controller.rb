a598d82d1f4bc2041c09cd3313d47304d6c7e762
Michael Knott <knottweb@gmail.com>
2021-03-11 01:45:45 +0000

    add segment controller methods

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -1,2 +1,60 @@
 class SegmentsController < ApplicationController
+  before_action :find_segment, only: [:show, :edit, :update, :destroy]
+  before_action :find_tour, only: [:new, :create, :show, :edit, :update, :destroy]
+
+  def index
+    @segments = Segment.all
+  end
+
+  def show
+    # code
+  end
+
+  def new
+    @segment = Segment.new
+  end
+
+  def create
+    @segment = Segment.new(segment_params)
+    @segment.tour = @tour
+    if @segment.save
+      redirect_to @tour
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    # code
+  end
+
+  def update
+    if @segment.update(segment_params)
+      redirect_to @segment
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @segment.destroy
+      redirect_to 'root'
+    else
+      redirect_to @segment
+    end
+  end
+
+  private
+
+  def segment_params
+    params.require(:segment).permit(:segment_date, :destination_office_id, :tour_id)
+  end
+
+  def find_segment
+    @segment = Segment.find(params[:id])
+  end
+
+  def find_tour
+    @tour = Tour.find(params[:tour_id])
+  end
 end
