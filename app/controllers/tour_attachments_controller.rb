33143e0a6a5931f666c7462d1895f9d2982822a9
Michael Knott <knottweb@gmail.com>
2021-03-11 15:03:22 +0000

    tour attachments controller

--- app/controllers/tour_attachments_controller.rb
+++ app/controllers/tour_attachments_controller.rb
@@ -1,2 +1,58 @@
 class TourAttachmentsController < ApplicationController
+  before_action :find_tour_attachment, only: [:show, :edit, :update, :destroy]
+  before_action :find_tour
+
+  def index
+    @tour_attachments = TourAttachments.all
+  end
+
+  def show
+    @tour_attachment = TourAttachment.find(params[:id])
+  end
+
+  def new
+    @tour_attachment = TourAttachment.new
+  end
+  
+  def create
+    @tour_attachment = TourAttachmentt.new(tour_attachments_params)
+    if @tour_attachment.save
+      redirect_to @tour_attachment
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tour_attachment.update(tour_attachment_params)
+      redirect_to @tour_attachment
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @tour_attachment.destroy
+      redirect_to 'root'
+    else
+      redirect_to @tour_attachment
+    end
+  end
+
+  private
+
+  def tour_attachments_params
+    params.require(:tour_attachments).permit(:type, :file, :tour_id)
+  end
+
+  def find_tour_attachment
+    @tour_attachment = TourAttachment.find(params[:id])
+  end
+
+  def find_tour
+    @tour = Tour.find(params[:tour_id])
+  end
 end
